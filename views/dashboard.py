5a93672b680ffb29ca7b44778a4f827b4e4987f6
James Iter <james.iter.cn@gmail.com>
2017-08-24 18:40:59 +0800

    加入磁盘吞吐量及IOPS的展现

--- views/dashboard.py
+++ views/dashboard.py
@@ -64,6 +64,27 @@ def show():
     for guest in guests_ret['data']:
         guests_mapping_by_uuid[guest['uuid']] = guest
 
+    disks_url = host_url + url_for('api_disks.r_get_by_filter')
+
+    disks_uuid = list()
+    for k, v in guests_current_top_10_ret['data'].items():
+        for item in v:
+            if 'disk_uuid' not in item:
+                break
+
+            if item['disk_uuid'] not in disks_uuid:
+                disks_uuid.append(item['disk_uuid'])
+
+    disks_url = disks_url + '?filter=uuid:in:' + ','.join(disks_uuid)
+
+    disks_ret = requests.get(url=disks_url)
+    disks_ret = json.loads(disks_ret.content)
+
+    # Disk uuid 与 Disk 的映射
+    disks_mapping_by_uuid = dict()
+    for disk in disks_ret['data']:
+        disks_mapping_by_uuid[disk['uuid']] = disk
+
     hosts_sum = {'cpu': 0, 'memory': 0}
 
     for host in hosts_ret['data']:
@@ -74,5 +95,5 @@ def show():
                            guests_distribute_count_ret=guests_distribute_count_ret,
                            disks_distribute_count_ret=disks_distribute_count_ret,
                            guests_current_top_10_ret=guests_current_top_10_ret,
-                           guests_mapping_by_uuid=guests_mapping_by_uuid)
+                           guests_mapping_by_uuid=guests_mapping_by_uuid, disks_mapping_by_uuid=disks_mapping_by_uuid)
 
