940c738c0287d04c92749277f23c03303aa20529
James Iter <james.iter.cn@gmail.com>
2017-12-28 21:23:36 +0800

    修正 request.form 写成 request.json 的bug，并美好初始化页面 磁盘类型 单选框

--- views/config.py
+++ views/config.py
@@ -51,20 +51,20 @@ def create():
             'gateway': request.form.get('gateway'),
             'dns1': request.form.get('dns1'),
             'dns2': request.form.get('dns2'),
-            'iops_base': int(request.json.get('iops_base', 1000)),
-            'iops_pre_unit': int(request.json.get('iops_pre_unit', 6)),
-            'iops_cap': int(request.json.get('iops_cap', 2000)),
-            'iops_max': int(request.json.get('iops_max', 3000)),
-            'iops_max_length': int(request.json.get('iops_max_length', 20)),
+            'iops_base': int(request.form.get('iops_base', 1000)),
+            'iops_pre_unit': int(request.form.get('iops_pre_unit', 6)),
+            'iops_cap': int(request.form.get('iops_cap', 2000)),
+            'iops_max': int(request.form.get('iops_max', 3000)),
+            'iops_max_length': int(request.form.get('iops_max_length', 20)),
             # 200 MiB
-            'bps_base': int(request.json.get('bps_base', 1024 * 1024 * 200)),
+            'bps_base': int(request.form.get('bps_base', 1024 * 1024 * 200)),
             # 0.1 MiB
-            'bps_pre_unit': int(request.json.get('bps_pre_unit', 1024 * 1024 / 10)),
+            'bps_pre_unit': int(request.form.get('bps_pre_unit', 1024 * 1024 / 10)),
             # 500 MiB
-            'bps_cap': int(request.json.get('bps_cap', 1024 * 1024 * 500)),
+            'bps_cap': int(request.form.get('bps_cap', 1024 * 1024 * 500)),
             # 1 GiB
-            'bps_max': int(request.json.get('bps_max', 1024 * 1024 * 1024)),
-            'bps_max_length': int(request.json.get('bps_max_length', 10))
+            'bps_max': int(request.form.get('bps_max', 1024 * 1024 * 1024)),
+            'bps_max_length': int(request.form.get('bps_max_length', 10))
         }
 
         url = host_url + '/api/config'
