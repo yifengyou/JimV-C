7def86c1a62860c6ead89a2112c672fb559e08c5
James Iter <james.iter.cn@gmail.com>
2018-07-20 01:03:49 +0800

    前端页面，系统配置页，辅助栏实现刷新虚拟机状态功能

--- misc/v0.5_to_v0.6/update.sql
+++ misc/v0.5_to_v0.6/update.sql
@@ -3,3 +3,5 @@ ALTER TABLE guest_cpu_memory CHANGE COLUMN memory_unused memory_rate TINYINT UNS
 
 ALTER TABLE guest_cpu_memory ADD INDEX (memory_available);
 ALTER TABLE guest_cpu_memory ADD INDEX (memory_rate);
+
+UPDATE snapshot SET status=status+1 WHERE status BETWEEN 1 AND 200;
