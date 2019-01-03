6b2dce0d1d898448568eb44d2d644b891409f5b3
James Iter <james.iter.cn@gmail.com>
2019-01-03 23:06:06 +0800

    走通升级脚本

--- misc/v0.7_to_v0.8/upgrade.sh
+++ misc/v0.7_to_v0.8/upgrade.sh
@@ -1,6 +1,32 @@
 #!/usr/bin/env bash
 
+systemctl stop jimvn
+systemctl stop jimvc
+
+deactivate
+mysql -u jimv -p < update.sql
+
 DEL S:IP:Used
 DEL S:IP:Available
 DEL S:VNCPort:Used
 DEL S:VNCPort:Available
+DEL H:GlobalConfig
+
+cat > /etc/yum.repos.d/JimV.repo << EOF
+[JimV]
+name=JimV - \$basearch
+baseurl=http://repo.jimv.cn/centos/7/os/\$basearch
+        http://repo.jimv.io/centos/7/os/\$basearch
+        http://repo.iit.im/centos/7/os/\$basearch
+failovermethod=priority
+enabled=1
+gpgcheck=0
+gpgkey=https://repo.jimv.cn/RPM-GPG-KEY-JIMV-114EA591
+EOF
+
+rm -f /etc/systemd/system/jimvc.service
+rm -rf /usr/local/venv-jimv
+rm -rf /usr/local/JimV-C
+
+yum install jimv-controller -y
+systemctl daemon-reload
